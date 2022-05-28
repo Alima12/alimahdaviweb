7bd5e2010f4f2b814972187f9d3652df44100265
Ali <aliali.ali1378@yahoo.com>
2022-05-28 21:15:01 +0430

    set domain

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import store from './store'
 import axios  from 'axios'
 import VueSweetalert2 from 'vue-sweetalert2';
 
-axios.defaults.baseURL = 'http://127.0.0.1:8000/';
+axios.defaults.baseURL = 'https://api.alimahdavimehr.ir/';
 
 
 createApp(App).use(store).use(VueSweetalert2).mount('#app')
