4529e357901e38638be2ddef6196fc4401286a6c
Alima <63489955+Alima12@users.noreply.github.com>
2022-06-01 15:08:47 +0430

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import store from './store'
 import axios  from 'axios'
 import VueSweetalert2 from 'vue-sweetalert2';
 
-axios.defaults.baseURL = 'https://api.alimahdavimehr.ir/';
+axios.defaults.baseURL = 'http://api.alimahdavimehr.ir/';
 
 
 createApp(App).use(store).use(VueSweetalert2).mount('#app')
